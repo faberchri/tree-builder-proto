064666da2a4c6813d6ecbcab79522211fe8551b6
fabian <faberchri@gmail.com>
2012-11-01 02:15:03 +0100

    Refactoring into multiple class files and packages

--- TreeBuilderProto/src/clusterer/TreeBuilder.java
+++ TreeBuilderProto/src/clusterer/TreeBuilder.java
@@ -8,18 +8,15 @@ import java.util.Random;
 import java.util.Set;
 
 
-public class TreeBuilder {
+public final class TreeBuilder {
 	
 	private static final int NUM_OF_USERS = 15;
 	private static final int NUM_OF_MOVIES = 10;
 
 	Double[][] randomMatrix = new Double[NUM_OF_MOVIES][NUM_OF_USERS];
 	
-	Set<Node> userNodes = new HashSet<Node>();	
-	Set<Node> movieNodes = new HashSet<Node>();
-	
-	private static int userNodeId = 0;
-	private static int movieNodeId = 0;
+	private Set<UserNode> userNodes = new HashSet<UserNode>();	
+	private Set<MovieNode> movieNodes = new HashSet<MovieNode>();
 	
 	
 	public TreeBuilder() {
@@ -44,7 +41,7 @@ public class TreeBuilder {
 		}
 	}
 	
-	private void printRandomMatrix() {
+	public void printRandomMatrix() {
 		for (int i = 0; i < randomMatrix.length; i++) {
 			for (int j = 0; j < randomMatrix[0].length; j++) {
 				System.out.print(randomMatrix[i][j] +  "\t| ");
@@ -54,16 +51,19 @@ public class TreeBuilder {
 	}
 	
 	private void initNodes() {
+		// create UserNode objects
 		List<UserNode> users = new ArrayList<UserNode>();
 		for (int i = 0; i < randomMatrix[0].length; i++) {
 			users.add(new UserNode());
 		}
+		
+		// create MovieNode objects
 		List<MovieNode> movies = new ArrayList<MovieNode>();
 		for (int i = 0; i < randomMatrix.length; i++) {
 			movies.add(new MovieNode());
 		}
 		
-		// set movieNodes to userNodes
+		// add movieNodes to userNodes
 		for (int i = 0; i < randomMatrix[0].length; i++) {
 			Map<MovieNode, Double> attributes = new HashMap<MovieNode, Double>();
 			for (int j = 0; j < randomMatrix.length; j++) {
@@ -75,7 +75,7 @@ public class TreeBuilder {
 			userNodes.add(users.get(i));
 		}
 		
-		// set userNodes to movieNodes
+		// add userNodes to movieNodes
 		for (int i = 0; i < randomMatrix.length; i++) {
 			Map<UserNode, Double> attributes = new HashMap<UserNode, Double>();
 			for (int j = 0; j < randomMatrix[0].length; j++) {
@@ -88,7 +88,7 @@ public class TreeBuilder {
 		}
 	}
 	
-	public Node[] getClosestNodes(Set<Node> openNodes) {
+	private Node[] getClosestNodes(Set<? extends Node> openNodes) {
 		double closesDistance = Double.MAX_VALUE;
 		Node[] closesNodes = new Node[2];
 		Set<Node> subSet = new HashSet<Node>(openNodes);
@@ -108,116 +108,30 @@ public class TreeBuilder {
 		return closesNodes;
 	}
 	
-	private void printAllNodesInSet(Set<Node> set, String nodeNames){
+	private void printAllNodesInSet(Set<? extends PrintableNode> set, String nodeNames){
 		System.out.println("-----------------------");
 		System.out.println(nodeNames);
-		for (Node node : set) {
+		for (PrintableNode node : set) {
 			System.out.println(node.toString()+"|\t"+node.getAttributesString());
 		}
 		System.out.println("-----------------------");
 	}
 	
-	public static void main(String[] args) {
-		TreeBuilder treeBuilder = new TreeBuilder();
-		treeBuilder.printRandomMatrix();
-		treeBuilder.printAllNodesInSet(treeBuilder.userNodes, "User Nodes:");
-		treeBuilder.printAllNodesInSet(treeBuilder.movieNodes, "MovieNodes:");
-		treeBuilder.getClosestNodes(treeBuilder.userNodes);
+	public void printAllOpenUserNodes() {
+		printAllNodesInSet(userNodes, "User Nodes:");
 	}
 	
-	private class UserNode extends AbstractNode implements Node {
-		
-		Map<MovieNode, Double> movies;
-		private final int id = userNodeId++;
-		
-		public UserNode() {		}
-		
-		public void setMovies(Map<MovieNode, Double> movies) {
-			this.movies = movies;
-		}
-		
-		public Set<MovieNode> getAttributeKeys() {
-			return movies.keySet();
-		}
-		
-		public Double getAttributeValue(Node node) {
-			return movies.get(node);
-		}
-				
-		public String getAttributesString() {
-			return getAttributesString(movies);
-		}
-		
-		@Override
-		public String toString() {
-			return "UserNode".concat(" ").concat(String.valueOf(id));
-		}
-		
+	public void printAllOpenMovieNodes() {
+		printAllNodesInSet(movieNodes, "MovieNodes:");
 	}
 	
-	private class MovieNode extends AbstractNode implements Node{
-		
-		Map<UserNode, Double> users;
-		private final int id = movieNodeId++;
-		
-		public MovieNode() { }
-		
-		public void setUsers(Map<UserNode, Double> users) {
-			this.users = users;
-		}
-		
-		public Set<UserNode> getAttributeKeys() {
-			return users.keySet();
-		}
-		
-		public Double getAttributeValue(Node node) {
-			return users.get(node);
-		}
-				
-		public String getAttributesString() {
-			return getAttributesString(users);
-		}
-		
-		@Override
-		public String toString() {
-			return "MovieNode".concat(" ").concat(String.valueOf(id));
-		}
+	public Node[] getClosestOpenUserNodes() {
+		return getClosestNodes(userNodes);
 	}
 	
-	public abstract class AbstractNode implements Node{
-		
-		public double getDistance(Node otherNode) {
-			Set<Node> union = new HashSet<Node>(this.getAttributeKeys());
-			union.addAll(otherNode.getAttributeKeys());
-			Set<Node> intersect = new HashSet<Node>(this.getAttributeKeys());
-			intersect.retainAll(otherNode.getAttributeKeys());
-			double summedRatingDiffs = 0.0;
-			for (Node userNode : intersect) {
-				summedRatingDiffs = Math.abs(this.getAttributeValue(userNode) - otherNode.getAttributeValue(userNode));
-			}
-			return 1.0 / (double)union.size() * summedRatingDiffs + ((double)union.size() - (double)intersect.size()) / (double)union.size();
-		}
-		
-		public String getAttributesString(Map<? extends AbstractNode, Double> map) {
-			String s = "";
-			for (Map.Entry<? extends AbstractNode, Double> entry : map.entrySet()) {
-				s = s.concat(entry.getKey().toString()).concat(": ").concat(entry.getValue().toString()).concat(";\t");
-			}
-			if (s.length() == 0) {
-				return "no_attributes";
-			} else {
-				return s.substring(0, s.length()-1);
-			}
-		}
-		
+	public Node[] getClosestOpenMovieNodes() {
+		return getClosestNodes(movieNodes);
 	}
 	
-	public interface Node {
-		public double getDistance(Node otherNode);
-		public Double getAttributeValue(Node node);
-		public Set<? extends AbstractNode> getAttributeKeys();
-		public String getAttributesString();
-	}
-	
-
+		
 }
