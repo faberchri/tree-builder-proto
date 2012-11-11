1875c03c8a0f85511a9c2429e6ea4bbccc300e07
danihegglin <dani.hegglin@gmail.com>
2012-11-11 18:27:07 +0100

    Update TreeBuilderProto/src/clusterer/NodeIdComparator.java

--- TreeBuilderProto/src/clusterer/NodeIdComparator.java
+++ TreeBuilderProto/src/clusterer/NodeIdComparator.java
@@ -7,8 +7,8 @@ public class NodeIdComparator implements Comparator<PrintableNode> {
 
 	@Override
 	public int compare(PrintableNode o1, PrintableNode o2) {
-		//return Integer.compare(o1.getId(), o2.getId());
-		return 1;
+		return Integer.compare(o1.getId(), o2.getId());
+		//return 1;
 	}
 
 }
