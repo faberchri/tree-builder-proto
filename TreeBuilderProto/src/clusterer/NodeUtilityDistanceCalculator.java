97a0ab2f0e8abba75748bc47092bc4b9d9118899
alessandramacri <alessandra.macri@hotmail.com>
2012-11-25 18:39:21 +0100

    Commit test

---
+++ TreeBuilderProto/src/clusterer/NodeUtilityDistanceCalculator.java
@@ -0,0 +1,14 @@
+package clusterer;
+
+public class NodeUtilityDistanceCalculator implements NodeDistanceCalculator {
+
+	
+	public double calculateDistance(Node n1, Node n2) {
+		// Calculates distance of two nodes based on Cobweb utility function
+		
+		
+		return 0;
+	}
+
+}
+
